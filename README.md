256f1168a6bf0b0afa598d690bf170c56d13ba40
Kristof Bastiaensen 3e <kba@3e.eu>
2022-12-12 16:34:26 +0100

    day4

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ My advent of code 2022 repository.  I am using different programming
 languages that I wanted to learn or explore better, or just use for
 fun :-)
 
-| Day | Language |
-|:----|:---------|
-| 1   | idris2   |
-| 2   | idris2   |
-| 3   | Python3  |
-|     |          |
+| Day | Language        |
+|:----|:----------------|
+| 1   | idris2          |
+| 2   | idris2          |
+| 3   | Python3         |
+| 4   | (gauche) scheme |
+|     |                 |
---
+++ day4/day4.scm
@@ -0,0 +1,49 @@
+(use scheme.regex)
+(use util.match)
+
+(define section-regex
+  (let ((capture-digits '($ (+ num))))
+    (regexp
+     `(: ,capture-digits "-"
+         ,capture-digits ","
+         ,capture-digits "-"
+         ,capture-digits))))
+
+(define (parse-section str)
+  (match (cdr (regexp-match->list
+               (regexp-matches section-regex str)))
+    [(s1 s2 s3 s4)
+     (list (cons (string->number s1)
+                 (string->number s2))
+           (cons (string->number s3)
+                 (string->number s4)))]))
+
+(define section-contains
+  (match-lambda* [((s1 . e1) (s2 . e2))
+                  (and (>= s2 s1) (<= e2 e1))]))
+
+(define section-overlaps
+  (match-lambda* [((s1 . e1) (s2 . e2))
+                  (and (<= s1 e2)
+                       (<= s2 e1))]))
+
+(define (read-sections-from-file file)
+  (map parse-section
+       (call-with-input-file file port->string-list)))
+
+(define (count-containments sections)
+  (count
+   (match-lambda [(s1 s2)
+                  (or (section-contains s1 s2) (section-contains s2 s1))])
+   sections))
+
+(define (count-overlaps sections)
+  (count
+   (match-lambda [(s1 s2)
+                  (or (section-overlaps s1 s2))])
+   sections))
+
+;; part1:
+;; (count-containments (read-sections-from-file "/tmp/input"))
+;; part2: 
+;; (count-overlaps (read-sections-from-file "/tmp/input"))
